9add9845c4dd399b57ae7fb681b14fb469cae1c2
Daniel Lasche <dlasche@gmail.com>
2021-06-07 21:31:41 +0000

    lab 11 with remove

---
+++ listmethods/listmeth02.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+proto = ["ssh", "http", "https"]
+
+protoa = ["ssh", "http", "https"]
+
+print(proto)
+
+proto.append("dns")
+protoa.append("dns")
+print(proto)
+proto2 = [22, 80, 443, 53]
+proto.extend(proto2)
+print(proto)
+protoa.append(proto2)
+print(protoa)
+
+proto.remove("https")
+print(proto)
+
