7f8e1154d0bfa4531f4a18c4888b6e20de87bade
Daniel Lasche <dlasche@gmail.com>
2021-06-07 19:43:15 +0000

    finished chall

---
+++ lab_input/chall.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+user_name = input("Please enter your name:")
+
+user_day = input("Please enter the day of the week:")
+
+print("Hello,", user_name, "Happy", user_day + "!")
+
