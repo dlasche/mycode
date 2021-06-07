566a7dbc8f5fe411e6b31a00a21d07d782ef6bbc
Daniel Lasche <dlasche@gmail.com>
2021-06-07 21:08:29 +0000

    lab 10

--- lab_input/chall.py
+++ lab_input/chall.py
@@ -6,3 +6,11 @@ user_day = input("Please enter the day of the week:")
 
 print("Hello,", user_name, "Happy", user_day + "!")
 
+# Print objects
+#print("Hello, ",name,"! Happy ",day,"!",sep="")
+# Concatenation
+#print("Hello, " + name + "! Happy " + day + "!")
+# Format Method
+#print("Hello, {}! Happy {}!".format(name,day))
+# F String
+#print(f"Hello, {name}! Happy {day}!")
---
+++ mix-list/mixlist01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3 
+
+#list
+my_list = ["192.168.0.5", 5060, "UP"]
+
+print("The first item in the list (IP): " + my_list[0])
+print("The second item in the list (port): " + str(my_list[1]))
+print("The third item in the list (state): " + my_list[2])
+
+#comment
+
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+print(f"The IP addresses for the second list are: {iplist[3]} and {iplist[4]}")
+
