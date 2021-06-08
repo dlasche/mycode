47bc35dd34f9616f919d3588bf0fb9111cce2c7e
Daniel Lasche <dlasche@gmail.com>
2021-06-08 17:14:09 +0000

    lab 17

---
+++ moveplease01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import shutil
+import os
+
+os.chdir('/home/student/mycode/')
+
+shutil.move('raynor.obj', 'ceph_storage')
+
+xname = input('What is the name for kerrigan.obj?')
+
+shutil.move('ceph_storage/kerrigan.obj', 'ceph_storage/' + xname)
+
